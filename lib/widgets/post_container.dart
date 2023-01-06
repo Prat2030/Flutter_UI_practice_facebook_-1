87757b5a58e3c3a2edd6f86ddba2f9bae672a160
Prat2030 <pratyushjindal2003@gmail.com>
2023-01-06 20:40:42 +0530

    completed posts container

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import '../config/palette.dart';
 import '../models/models.dart';
 import './widgets.dart';
 
@@ -39,6 +40,10 @@ class PostContainer extends StatelessWidget {
                   child: Image.network(post.imageUrl!),
                 )
               : const SizedBox.shrink(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: _PostStats(post: post),
+          ),
         ],
       ),
     );
@@ -90,6 +95,129 @@ class _PostHeader extends StatelessWidget {
           onPressed: () => print('More'),
         ),
       ],
+    );
+  }
+}
+
+class _PostStats extends StatelessWidget {
+  final Post post;
+  const _PostStats({
+    Key? key,
+    required this.post,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(4.0),
+              decoration: BoxDecoration(
+                color: Palette.facebookBlue,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(
+                Icons.thumb_up,
+                size: 10.0,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(width: 4.0),
+            Expanded(
+              child: Text(
+                '${post.likes}',
+                style: TextStyle(
+                  color: Colors.grey[600],
+                ),
+              ),
+            ),
+            const SizedBox(width: 8.0),
+            Text(
+              '${post.comments} Comments',
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
+            ),
+            const SizedBox(width: 8.0),
+            Text(
+              '${post.shares} Shares',
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
+        const Divider(),
+        Row(
+          children: [
+            _PostButton(
+              icon: Icon(
+                Icons.thumb_up,
+                color: Colors.blue[600],
+                size: 20.0,
+              ),
+              label: 'Like',
+              onTap: () => print('Like'),
+            ),
+            _PostButton(
+              icon: Icon(
+                Icons.comment,
+                color: Colors.grey[600],
+                size: 20.0,
+              ),
+              label: 'Comment',
+              onTap: () => print('Comment'),
+            ),
+            _PostButton(
+              icon: Icon(
+                Icons.share,
+                color: Colors.grey[600],
+                size: 25.0,
+              ),
+              label: 'Share',
+              onTap: () => print('Share'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _PostButton extends StatelessWidget {
+  final Icon icon;
+  final String label;
+  final VoidCallback onTap;
+  const _PostButton({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        color: Colors.white,
+        child: InkWell(
+          onTap: onTap,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            height: 25.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icon,
+                const SizedBox(width: 4.0),
+                Text(label),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
