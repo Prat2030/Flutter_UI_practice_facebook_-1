fdea4fb4674bc6b72bc5e6414adc61132038df74
Prat2030 <pratyushjindal2003@gmail.com>
2023-01-06 18:33:07 +0530

    commit

--- lib/widgets/profile_avatar.dart
+++ lib/widgets/profile_avatar.dart
@@ -20,20 +20,21 @@ class ProfileAvatar extends StatelessWidget {
           backgroundColor: Colors.grey[200],
           backgroundImage: CachedNetworkImageProvider(imageUrl),
         ),
-        if (isActive)
-          Positioned(
-            bottom: 0.0,
-            right: 0.0,
-            child: Container(
-              height: 15.0,
-              width: 15.0,
-              decoration: BoxDecoration(
-                color: Colors.green,
-                shape: BoxShape.circle,
-                border: Border.all(width: 2.0, color: Colors.white),
-              ),
-            ),
-          ),
+        isActive
+            ? Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  height: 15.0,
+                  width: 15.0,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 2.0, color: Colors.white),
+                  ),
+                ),
+              )
+            : const SizedBox.shrink(),
       ],
     );
   }
