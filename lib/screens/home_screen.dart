646704b917e1b817a9ba1ebe086b992286d87ddd
Prat2030 <pratyushjindal2003@gmail.com>
2023-01-07 21:38:49 +0530

    commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,66 +12,125 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            brightness: Brightness.light,
-            backgroundColor: Colors.white,
-            title: Text(
-              'facebook',
-              style: const TextStyle(
-                color: Palette.facebookBlue,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: -1.2,
-              ),
-            ),
-            centerTitle: false,
-            floating: true,
-            actions: [
-              CircleButton(
-                  icon: Icons.search,
-                  iconSize: 30.0,
-                  onPressed: () => print('Search')),
-              CircleButton(
-                  icon: MdiIcons.facebookMessenger,
-                  iconSize: 30.0,
-                  onPressed: () => print('Messenger')),
-            ],
-          ),
-          SliverToBoxAdapter(
-            // SliverPadding,Sliverlists,SliverGrids can also be used
-            child: CreatePostContainer(
-              currentUser: currentUser,
-            ),
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
-            sliver: SliverToBoxAdapter(
-              child: Rooms(onlineUsers: onlineUsers),
-            ),
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
-            sliver: SliverToBoxAdapter(
-              child: Stories(
-                currentUser: currentUser,
-                stories: stories,
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                final Post post = posts[index];
-                return PostContainer(post: post);
-              },
-              childCount: posts.length,
-            ),
-          ),
-        ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Responsive(
+          mobile: _HomeScreenMobile(),
+          desktop: _HomeScreenDesktop(),
+        ),
       ),
+    );
+  }
+}
+
+class _HomeScreenMobile extends StatelessWidget {
+  const _HomeScreenMobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          brightness: Brightness.light,
+          backgroundColor: Colors.white,
+          title: Text(
+            'facebook',
+            style: const TextStyle(
+              color: Palette.facebookBlue,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: -1.2,
+            ),
+          ),
+          centerTitle: false,
+          floating: true,
+          actions: [
+            CircleButton(
+                icon: Icons.search,
+                iconSize: 30.0,
+                onPressed: () => print('Search')),
+            CircleButton(
+                icon: MdiIcons.facebookMessenger,
+                iconSize: 30.0,
+                onPressed: () => print('Messenger')),
+          ],
+        ),
+        SliverToBoxAdapter(
+          // SliverPadding,Sliverlists,SliverGrids can also be used
+          child: CreatePostContainer(
+            currentUser: currentUser,
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+          sliver: SliverToBoxAdapter(
+            child: Rooms(onlineUsers: onlineUsers),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final Post post = posts[index];
+              return PostContainer(post: post);
+            },
+            childCount: posts.length,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _HomeScreenDesktop extends StatelessWidget {
+  const _HomeScreenDesktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            color: Colors.red,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text('Facebook'),
+              ),
+            ),
+          ),
+        ),
+        const Spacer(),
+        Container(
+          width: 600.0,
+          child: CustomTabBar(
+            icons: const [
+              Icons.home,
+              Icons.ondemand_video,
+              Icons.storefront,
+              Icons.group,
+              Icons.notifications,
+            ],
+            selectedIndex: 0,
+            onTap: (index) => print(index),
+            isBottomIndicator: true,
+          ),
+        ),
+        const Spacer(),
+        Expanded(
+          child: Container(
+            color: Colors.blue,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text('User'),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
